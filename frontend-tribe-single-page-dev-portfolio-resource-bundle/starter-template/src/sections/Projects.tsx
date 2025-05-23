f89f9ab0e63665e82a204ac662277ee06c3ee89d
aseel <aseel26005@gmail.com>
2025-05-24 01:38:55 +0530

    first commit

---
+++ frontend-tribe-single-page-dev-portfolio-resource-bundle/starter-template/src/sections/Projects.tsx
@@ -0,0 +1,103 @@
+import darkSaasLandingPage from "@/assets/images/ScrapCraft.jpg";
+import lightSaasLandingPage from "@/assets/images/SuperTees.png";
+import aiStartupLandingPage from "@/assets/images/NewsQuest.png";
+import Image from "next/image";
+import CheckCircleIcon from '@/assets/icons/check-circle.svg';
+import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg';
+import grainImage from '@/assets/images/grain.jpg';
+import { SectionHeader } from "@/components/SectionHeader";
+import { Card } from "@/components/Card";
+
+const portfolioProjects = [
+  {
+    company: "Minor-Project",
+    year: "2024",
+    title: "Scrap Craft AI Hub",
+    results: [
+      { title: "Enhanced user experience by 40%" },
+      { title: "Improved site speed by 50%" },
+      { title: "Increased mobile traffic by 35%" },
+    ],
+    link: "https://drive.google.com/file/d/1mWI4NT_sqsl2nuEDCAU4qOnWUlgXHo2Q/view?usp=sharing",
+    image: darkSaasLandingPage,
+  },
+  {
+    company: "Zidio",
+    year: "2025",
+    title: "SuperTees: E-commerce Platform",
+    results: [
+      { title: "Boosted sales by 20%" },
+      { title: "Expanded customer reach by 35%" },
+      { title: "Increased brand awareness by 15%" },
+    ],
+    link: "https://drive.google.com/file/d/1Og8_XZh6DqRMY4VbPmnSs6Q0eTjO4Hnn/view?usp=sharing",
+    image: lightSaasLandingPage,
+  },
+  {
+    company: "Mini-Project",
+    year: "2025",
+    title: "NewsQuest Hub",
+    results: [
+      { title: "Enhanced user experience by 40%" },
+      { title: "Improved site speed by 50%" },
+      { title: "Increased mobile traffic by 35%" },
+    ],
+    link: "https://drive.google.com/file/d/1V2jLNZEf-gBQ_hQUeM8L14tn_DHkcBL0/view?usp=sharing",
+    image: aiStartupLandingPage,
+  },
+];
+
+export const ProjectsSection = () => {
+  return (
+  <section id="projects" className="pb-16 lg:py-24">
+    <div className="container">
+      <SectionHeader 
+      eyebrow="Real-World Results" 
+      title=" Featured Projects" 
+      description="See how I transformed concepts into engaging digital experiences."/>
+      
+      <div className="flex flex-col mt-10 md:mt-20 gap-20">
+        {portfolioProjects.map((project, projectIndex)=> (
+          <Card
+            key ={project.title} 
+            className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky" 
+            style={{
+              top: `calc(64px + ${projectIndex * 40}px)`,
+              
+            }}> 
+            
+            <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+              <div className="lg:pb-16">
+              <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+              <span>{project.company}</span>
+              <span>&bull;</span>
+              <span>{project.year}</span>
+              </div>
+            
+            <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">{project.title}</h3>
+            <hr className="border-t-2 border-white/5 mt-4 md:mt-5"/>
+            <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+              {project.results.map(result => (
+                <li className="flex gap-2 text-sm md:text-base text-white/50">
+                  <CheckCircleIcon className="size-5 md:size-6" />
+                  <span>{result.title}</span></li>
+              ))}
+            </ul>
+            <a href={project.link}>
+             <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+               <span>Visit Live Site</span>
+               <ArrowUpRightIcon  className="size-4"/>
+              </button>
+            </a>
+            </div>
+            <div className="relative">
+            <Image src={project.image} alt={project.title} className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto  lg:max-w-none" />
+            </div>
+            </div>
+          </Card>
+        ))}
+      </div>
+    </div>
+  </section>
+  );
+};
